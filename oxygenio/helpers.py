724140566c7bd0d07826be44b8e5ad909b6f4305
EsauM10 <esau2096@gmail.com>
2024-01-24 12:24:20 -0300

    feat: add create command

--- oxygenio/helpers.py
+++ oxygenio/helpers.py
@@ -11,3 +11,11 @@ def create_app(app_name: str, template_folder: str, static_folder: str) -> Flask
     app = Flask(app_name, template_folder=template_folder, static_folder=static_folder)
     app.add_url_rule('/', view_func=_root)
     return app
+
+def create_file(filename: str, data: str):
+    with open(filename, mode='w') as file:
+        file.write(data)
+
+def read_file(filename: str) -> str:
+    with open(filename, mode='r') as file:
+        return file.read()
--- oxygenio/scripts.py
+++ oxygenio/scripts.py
@@ -1,6 +1,9 @@
 import argparse
+import json
 import os
 from pathlib import Path
+
+from oxygenio.helpers import create_file, read_file
 
 BASEDIR  = str(Path(__file__).parent)
 DATA_DIR = os.path.join(BASEDIR, 'data')
@@ -14,9 +17,27 @@ def build():
     with open(filename, mode='r') as file:
         print(file.read())
 
+def create():
+    data = {
+        'mode': 'dev',
+        'devCommand': 'npm run dev',
+        'buildCommand': 'npm run build',
+        'appURL': 'http://localhost:5173',
+        'distFolder': 'dist'
+    }
+    print('Creating config.json 📂...')
+    create_file('config.json', json.dumps(data, indent=4))
+
+    print('Creating main.py 📄...')
+    sample_file = os.path.join(DATA_DIR, 'sample.txt')
+    create_file('main.py', read_file(sample_file))
+    
+    
+
 def main():
     subparsers.add_parser('build', help='builds the executable file').set_defaults(func=build)
-
+    subparsers.add_parser('create', help='create all the necessaries files').set_defaults(func=create)
+    
     args = global_parser.parse_args()
     args.func()
 
