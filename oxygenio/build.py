1ae0bdf7b78a5982fd6d1c8eaf097c5a89c74965
EsauM10 <esau2096@gmail.com>
2024-02-12 22:01:27 -0300

    feat: add dynamic static folder

--- oxygenio/build.py
+++ oxygenio/build.py
@@ -3,12 +3,10 @@ import os
 import shutil
 import tempfile
 
-from bs4 import BeautifulSoup
-
 from oxygenio.config import ConfigLoader
 from oxygenio.helpers import (
     CONFIG_FILENAME,
-    read_file, create_file, run_command
+    create_file, run_command
 )
 
 class ViteBuilder:
@@ -20,26 +18,13 @@ class ViteBuilder:
         data['mode'] = 'build'
         create_file(filename, json.dumps(data, indent=4))
 
-    def create_index_file(self, filename: str, assets_folder: str):
-        index_file = os.path.join(self.config.dist_path, 'index.html')
-        html_content = self.update_index_file(index_file, assets_folder)
-        create_file(filename, html_content)
-
-    def update_index_file(self, filename: str, assets_folder: str) -> str:
-        assets_filenames = os.listdir(assets_folder)
-        javascript_file = list(filter(lambda file: file.endswith('.js'), assets_filenames))[0]
-        css_file = list(filter(lambda file: file.endswith('.css'), assets_filenames))[0]
-
-        if(not javascript_file.endswith('.js')):
-            javascript_file = assets_filenames[1]
-            css_file = assets_filenames[0]
-        
-        parser = BeautifulSoup(read_file(filename), 'html.parser')
-        script_tag = parser.find('script', src=f'/assets/{javascript_file}')
-        link_tag = parser.find('link', href=f'/assets/{css_file}')
-        script_tag['src'] = "{{ " + f"url_for('static', filename='{javascript_file}')" + " }}" # type: ignore
-        link_tag['href'] = "{{ " + f"url_for('static', filename='{css_file}')" + " }}" # type: ignore
-        return parser.prettify()
+    def create_templates_folder(self, templates_path: str):
+        os.mkdir(templates_path)
+        for filename in os.listdir(self.config.dist_path):
+            file = os.path.join(self.config.dist_path, filename)
+            if(os.path.isfile(file)):
+                destination = os.path.join(templates_path, filename)
+                shutil.copyfile(file, destination)
 
     def build(self):
         run_command(self.config.build_command.split(' '))
@@ -48,27 +33,24 @@ class ViteBuilder:
         try:
             assets_folder = os.path.join(self.config.dist_path, 'assets')
             config_file = os.path.join(tempdir.name, CONFIG_FILENAME)
-            static_folder = os.path.join(tempdir.name, 'static')
-            templates_folder = os.path.join(tempdir.name, 'templates')
-            html_file = os.path.join(templates_folder, 'index.html')
+            static_temp_folder = os.path.join(tempdir.name, 'static')
+            templates_temp_folder = os.path.join(tempdir.name, 'templates')
             
-            os.mkdir(templates_folder)
-            shutil.copytree(src=assets_folder, dst=static_folder)
-            
-            self.create_index_file(html_file, assets_folder)
+            shutil.copytree(src=assets_folder, dst=static_temp_folder)
+            self.create_templates_folder(templates_temp_folder)
             self.create_config_file(config_file)
 
             run_command([
                 'pyinstaller', '--noconfirm', '--onefile', '--windowed', '--clean',
                 f'--add-data={config_file}:.',
-                f'--add-data={static_folder}:static',
-                f'--add-data={templates_folder}:templates',
+                f'--add-data={static_temp_folder}:{self.config.static_folder}',
+                f'--add-data={templates_temp_folder}:templates',
                 '--hidden-import=engineio.async_drivers.gevent',
                 'main.py'
             ])
         except Exception as ex:
             raise ex
         finally:
-            print('Cleanup directory: 🧹', tempdir.name)
+            print(f'Cleanup directory: {tempdir.name} 🧹')
             tempdir.cleanup()
             os.remove('main.spec')
