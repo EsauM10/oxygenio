e3fadf3734f98c3379d24f6d90cb5cd2b27fb893
EsauM10 <esau2096@gmail.com>
2026-07-26 21:22:08 -0300

    fix: reliably start server and close app when the embedded browser is closed

--- oxygenio/browsers.py
+++ oxygenio/browsers.py
@@ -1,26 +1,55 @@
+import json
+import os
+import tempfile
 from abc import ABC, abstractmethod
-from subprocess import Popen, PIPE
+from subprocess import Popen
 from typing import Literal
-
-from oxygenio.helpers import run_command
 
 BrowserName = Literal['chrome', 'edge']
 
+def _create_isolated_profile(prefix: str) -> str:
+    user_data_dir = tempfile.mkdtemp(prefix=prefix)
+    profile_dir = os.path.join(user_data_dir, 'Default')
+    os.makedirs(profile_dir, exist_ok=True)
+
+    preferences_path = os.path.join(profile_dir, 'Preferences')
+    with open(preferences_path, mode='w') as file:
+        json.dump({'translate': {'enabled': False}}, file)
+
+    return user_data_dir
+
 class Browser(ABC):
     @abstractmethod
-    def run(self, url: str):
+    def run(self, url: str) -> Popen:
         pass
 
 class Chrome(Browser):
     def __init__(self) -> None:
         self.path = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
 
-    def run(self, url: str):
-        Popen(
-            [self.path, f'--app={url}'], 
-            stdout=PIPE, stderr=PIPE, stdin=PIPE
+    def run(self, url: str) -> Popen:
+        user_data_dir = _create_isolated_profile('oxygenio-chrome-')
+        return Popen(
+            [
+                self.path,
+                f'--user-data-dir={user_data_dir}',
+                '--disable-features=Translate,TranslateUI',
+                f'--app={url}'
+            ]
         )
 
 class Edge(Browser):
-    def run(self, url: str):
-        run_command(['start', 'msedge', '--new-window', f'--app={url}'])
+    def __init__(self) -> None:
+        self.path = 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
+
+    def run(self, url: str) -> Popen:
+        user_data_dir = _create_isolated_profile('oxygenio-edge-')
+        return Popen(
+            [
+                self.path,
+                f'--user-data-dir={user_data_dir}',
+                '--disable-features=Translate,TranslateUI',
+                '--new-window',
+                f'--app={url}'
+            ]
+        )
--- oxygenio/server.py
+++ oxygenio/server.py
@@ -1,6 +1,10 @@
 from gevent import monkey
 monkey.patch_all()
 
+import gevent
+import socket
+import time
+from subprocess import Popen
 from typing import Any, Callable, Type
 
 from flask_socketio import SocketIO
@@ -39,20 +43,40 @@ class Oxygenio:
         if(not self.config.is_dev_mode):
             self.__socketio.on_event('onclose', self.__socketio.stop) # type: ignore
 
-    def run(self, host: str = 'localhost', port: int = 15999, browser: BrowserName = 'chrome'):
+    def _watch_browser(self, process: Popen):
+        process.wait()
+        self.__socketio.stop()
+
+    def _wait_until_listening(self, host: str, port: int, timeout: float = 10):
+        deadline = time.time() + timeout
+        while(time.time() < deadline):
+            try:
+                with socket.create_connection((host, port), timeout=0.5):
+                    return
+            except OSError:
+                gevent.sleep(0.1)
+        raise RuntimeError(f'Server did not start listening on {host}:{port} in time')
+
+    def run(self, host: str = '127.0.0.1', port: int = 15999, browser: BrowserName = 'chrome'):
         enable = self.config.is_dev_mode
 
         app_url = self._get_app_url(host, port)
-        if(not self.config.is_dev_mode):
-            browsers[browser]().run(app_url)
-
         app = create_app('__main__', static_folder=self.config.static_folder)
         self.__socketio.init_app(app, cors_allowed_origins=app_url)
-        self.__socketio.run(
-            app, 
-            host=host, 
-            port=port, 
-            debug=enable, 
-            use_reloader=enable, 
+
+        server = gevent.spawn(
+            self.__socketio.run,
+            app,
+            host=host,
+            port=port,
+            debug=enable,
+            use_reloader=enable,
             log_output=False
         )
+
+        if(not self.config.is_dev_mode):
+            self._wait_until_listening(host, port)
+            process = browsers[browser]().run(app_url)
+            gevent.spawn(self._watch_browser, process)
+
+        server.join()
